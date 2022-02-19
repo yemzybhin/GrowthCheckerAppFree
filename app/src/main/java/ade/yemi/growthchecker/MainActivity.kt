304defeaf20ea1b15a8ac56e5de47426e60f2eb9
yemzybhin <63170357+yemzybhin@users.noreply.github.com>
2022-02-19 21:53:10 +0100

    Project Start

---
+++ app/src/main/java/ade/yemi/growthchecker/MainActivity.kt
@@ -0,0 +1,11 @@
+package ade.yemi.growthchecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
